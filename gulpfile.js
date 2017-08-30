b1e031a276949b9883135c286dbe187bbc0396bb
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-08-30 20:24:38 +0300

    set gulp to live reload on changes

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp')
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('browser-sync',function(){
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch("./css/*.css").on('change', browserSync.reload);
+  gulp.watch("./js/*.js").on('change', browserSync.reload);
+});
