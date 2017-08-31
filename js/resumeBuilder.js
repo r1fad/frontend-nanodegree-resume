c025cb99d6149e9aa0bf58238b4c655620a99e65
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-08-31 15:14:56 +0300

    add objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,37 @@
+var work = {
+  "jobs":[
+    {
+      "employer":"CSSoc",
+      "title": "President",
+      "location": "Manchester"
+      "description": "Society lead"
+    }
+  ]
+}
 
+var projects = {
+  "projects": [
+    {
+      "title": "MancBot",
+      "dates": "September 2016 - June 2017",
+      "description": "messenger bot"
+      "images": [
+        "../images/fry.jpg"
+      ]
+    }
+  ]
+}
+
+var bio = {
+  "name": "Rifad Lafir",
+  "role": "Software Engineer",
+  "welcomeMessage" : "Hello!",
+  "biopic": "../images/fry.jpg",
+  "skills": ["programming"]
+  "contacts":{
+    "mobile": "07849514226",
+    "email": "rifad@email.com"
+    "github": "r1fad"
+    "location":"Manchester, UK"
+  }
+}
